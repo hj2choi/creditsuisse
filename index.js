82b105e99e151dcc0a9c303c48beed6cbb40cf9b
hjchoi95 <hongjoonchoi95@gmail.com>
2016-10-01 11:36:57 +0800

    body

--- index.js
+++ index.js
@@ -1,28 +1,42 @@
+var TEAM_UID = "dmdx-gZILTVhit9u-kZ42Q"
+
 var express = require('express')
 var request = require('request')
 var app = express()
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
-/*
-request('http://cis2016-exchange1.herokuapp.com/api/market_data', function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    console.log(body) // Show the HTML for the Google homepage.
-  }
-})
 
-request.get('http://cis2016-exchange1.herokuapp.com/api/market_data').on('response', function(error, response, body) {
-    //console.log(response.statusCode) // 200
-    //console.log(response.headers['content-type']) // 'image/png'
-    console.log(body);
-  })
-*/
+
+
+
+
+
+var buyRequest = {
+	"team_uid":TEAM_UID,
+	"side":"buy",
+	"symbol": "0388",
+	"ask": 192,
+	"qty": 10,
+	"order_type":"market"
+};
+
 app.get('/', function(req, res) {
-  request('http://cis2016-exchange1.herokuapp.com/api/market_data', function (error, response, body) {
+  request.get('http://cis2016-exchange1.herokuapp.com/api/market_data', function (error, response, body) {
     if (!error && response.statusCode == 200) {
+      console.log("market_data:");
       console.log(body) // Show the HTML for the Google homepage.
     }
   })
+  request.post('http://cis2016-exchange1.herokuapp.com/api/orders', buyRequest, function (error, response, body) {
+    console.log("sending buy request: ");
+    if (!error && response.statusCode == 200) {
+      console.log(body) // Show the HTML for the Google homepage.
+    } else {
+      console.log("ERR: "+error);
+    }
+  })
+  res.send("done");
 
 })
 
