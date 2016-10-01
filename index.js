7b56f2ef98f78960bb592a1cef3d19fdb5806de9
hjchoi95 <hongjoonchoi95@gmail.com>
2016-10-01 14:22:44 +0800

    all market data

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-var TEAM_UID = "yPlWaf6NAoM6yjTTEs4_5g"
+var TEAM_UID = "yPlWaf6NAoM6yjTTEs4_5g";
+var SYMBOLS = ["0005","0001","0388","0386","3988"];
 
 var express = require('express')
 var request = require('request')
@@ -8,17 +9,49 @@ var path = require('path')
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-var market_data;
+var stocks = {"0005":[{qty:1, price:5},{qty:3, price:15}],"0001":[{qty:1, price:5},{qty:3, price:15}]};
 
+var team_data;
+var market_data_1;
+var market_data_2;
+var market_data_3;
+var historical_market_data_1;
+var historical_market_data_2;
+var historical_market_data_3;
 
-///////////////////////////////////////////////////////////////////
-// functions
-///////////////////////////////////////////////////////////////////
-function pullMarketData(callback) {
+//////////////////////////////////////////////////////////////////////////////////////////////
+// helper functions
+//////////////////////////////////////////////////////////////////////////////////////////////
+function pullMarketData_1(callback) {
   request.get('http://cis2016-exchange1.herokuapp.com/api/market_data', function (error, response, body) {
     if (!error && response.statusCode == 200) {
-      console.log("pulled market_data");
-      //console.log(body) // Show the HTML for the Google homepage.
+      //console.log("pulled market_data_1");
+      callback(body);
+    }
+  })
+}
+function pullMarketData_2(callback) {
+  request.get('http://cis2016-exchange2.herokuapp.com/api/market_data', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      //console.log("pulled market_data_2");
+      callback(body);
+    }
+  })
+}
+function pullMarketData_3(callback) {
+  request.get('http://cis2016-exchange3.herokuapp.com/api/market_data', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      //console.log("pulled market_data_3");
+      callback(body);
+    }
+  })
+}
+
+
+function pullTeamData(callback) {
+  request.get('http://cis2016-teamtracker.herokuapp.com/api/teams/'+TEAM_UID, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      console.log("pulled team_data");
       callback(body);
     }
   })
@@ -29,7 +62,6 @@ function createBuyRequest(symbol, ask, qty, limit, callback) {
   if (limit) {
     order_type = "limit";
   }
-
   var requestData = {
   	"team_uid":TEAM_UID,
   	"side":"buy",
@@ -39,11 +71,14 @@ function createBuyRequest(symbol, ask, qty, limit, callback) {
   	"qty": qty,
   	"order_type":order_type
   };
-
   request.post({url:'http://cis2016-exchange1.herokuapp.com/api/orders', formData:requestData}, function (error, response, body) {
     console.log("createBuyRequest(): "+order_type+"_"+symbol+" $"+ask+" x"+qty);
     if (!error && response.statusCode == 200) {
       //console.log(body) // Show the HTML for the Google homepage.
+      pullTeamData(function(){});
+      //TODO: update stock data here. body is a response data.
+
+
       callback(body);
     } else {
       console.log("ERR createBuyRequest(): "+error);
@@ -51,13 +86,11 @@ function createBuyRequest(symbol, ask, qty, limit, callback) {
   })
 }
 
-
 function createSellRequest(symbol, bid, qty, limit, callback) {
   var order_type = "market";
   if (limit) {
     order_type = "limit";
   }
-
   var requestData = {
   	"team_uid":TEAM_UID,
   	"side":"sell",
@@ -67,11 +100,14 @@ function createSellRequest(symbol, bid, qty, limit, callback) {
   	"qty": qty,
   	"order_type":order_type
   };
-
   request.post({url:'http://cis2016-exchange1.herokuapp.com/api/orders', formData:requestData}, function (error, response, body) {
     console.log("createSellRequest(): "+order_type+"_"+symbol+" $"+bid+" x"+qty);
     if (!error && response.statusCode == 200) {
       //console.log(body) // Show the HTML for the Google homepage.\
+      pullTeamData(function(){});
+      //TODO: update stock data here. body is a response data.
+
+
       callback(body);
     } else {
       console.log("ERR createSellRequest(): "+error);
@@ -79,49 +115,70 @@ function createSellRequest(symbol, bid, qty, limit, callback) {
   })
 }
 
-
-app.get('/bashboard', function(req,res) {
-	res.sendFile(path.join(__dirname+'/view/bashboard.html'));
-})
-
-app.get('/api/market_data', function(req, res) {
-  res.send(market_data);
+//////////////////////////////////////////////////////////////////////////////////////////////
+// automated scripts
+//////////////////////////////////////////////////////////////////////////////////////////////
+function repeatPullMarketData_1(data) {
+  market_data_1=data;
+  pullMarketData_1(repeatPullMarketData_1);
+}
+pullMarketData_1(function(data) {
+  repeatPullMarketData_1(data);
 });
 
+function repeatPullMarketData_2(data) {
+  market_data_2=data;
+  pullMarketData_2(repeatPullMarketData_2);
+}
+pullMarketData_2(function(data) {
+  repeatPullMarketData_2(data);
+});
+
+function repeatPullMarketData_3(data) {
+  market_data_3=data;
+  pullMarketData_3(repeatPullMarketData_3);
+}
+pullMarketData_3(function(data) {
+  repeatPullMarketData_3(data);
+});
+
+
+// team data every 5 secs, also after buy or sell
+function repeatPullTeamData(data) {
+  team_data=data;
+  setTimeout(pullTeamData,5000,repeatPullTeamData);
+}
+pullTeamData(function(data){
+  repeatPullTeamData();
+});
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+// front-end APIs
+//////////////////////////////////////////////////////////////////////////////////////////////
+app.get('/bashboard', function(req,res) {
+	res.sendFile(path.join(__dirname+'/view/bashboard.html'));
+});
+
+app.get('/api/market_data_1', function(req, res) {
+  res.send(market_data_1);
+});
+app.get('/api/market_data_2', function(req, res) {
+  res.send(market_data_2);
+});
+app.get('/api/market_data_3', function(req, res) {
+  res.send(market_data_3);
+});
+
+app.get('/api/team_data', function(req, res) {
+  res.send(team_data);
+});
+// remove costly stocks first!
+
 app.get('/', function(req, res) {
-  // request.get('http://cis2016-exchange1.herokuapp.com/api/market_data', function (error, response, body) {
-  //   if (!error && response.statusCode == 200) {
-  //     console.log("market_data:");
-  //     console.log(body) // Show the HTML for the Google homepage.
-  //   }
-  // })
-  // request.post({url:'http://cis2016-exchange1.herokuapp.com/api/orders', formData:buyRequest}, function (error, response, body) {
-  //   console.log("sending buy request: ");
-  //   if (!error && response.statusCode == 200) {
-  //     console.log(body) // Show the HTML for the Google homepage.
-  //   } else {
-  //     console.log("ERR: "+error);
-  //   }
-  // })
-  /*pullMarketData(function(data){console.log(data)});
-  createBuyRequest("0001",103,5,false,function(data){
-    console.log(data);
-    createSellRequest("0001",105,5,false,function(data){console.log(data)});
-  });*/
-
+  createBuyRequest("0001", 113, 3, false,function(data){console.log(data);});
   res.send("done");
-
-})
+});
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.	get('port'))
-})
-
-
-function repeatPullMarketData(data) {
-  market_data=data;
-  pullMarketData(repeatPullMarketData);
-}
-pullMarketData(function(data) {
-  repeatPullMarketData(data);
 });
